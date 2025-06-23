7d759a5698cd40a81f6ace8ae18ee0c6eefe1ef2
Yash Kathe <yashkathe123@gmail.com>
2025-06-23 13:42:07 -0400

    sync c51ee30a43f9c31d617fb1d414feee1bca9fc52fcb001f9a8f00048bea708023

--- nvim/lua/vim-config.lua
+++ nvim/lua/vim-config.lua
@@ -1,8 +1,13 @@
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=7")
+vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set termguicolors")
+
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.cmd("filetype plugin indent on")
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.g.mapleader = " "
